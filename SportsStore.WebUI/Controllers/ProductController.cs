e4636a0c1e4b1c86689a3d75051c85188a13d101
MULLERDES <amelnikd@gmail.com>
2016-07-31 18:57:28 +0300

    SS-201607311857 Instant -> pagination

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.ViewModels;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController :Controller
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        private const int PageSize = 4;
+        public ActionResult List(int page = 1)
+        {
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = _productRepository.Products.OrderBy(p => p.Id).Skip((page - 1) * PageSize).Take(PageSize).ToList(),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _productRepository.Products.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
