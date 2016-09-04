8dd2064415f0b030c53ff9b24cc3b36169baa84e
MULLERDES <amelnikd@gmail.com>
2016-09-04 16:22:06 +0300

    Implement forms auth

--- SportsStore.WebUI/Controllers/AccountController.cs
+++ SportsStore.WebUI/Controllers/AccountController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.ViewModels;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private IAuthProvider _authProvider;
+
+        public AccountController(IAuthProvider authProvider)
+        {
+            _authProvider = authProvider;
+        }
+
         // GET: Account
         public ViewResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
